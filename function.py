7be4be4fe1e815b2859da8f47a407a462a503753
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-04 08:35:25 +0100

    day4

--- function.py
+++ function.py
@@ -4,8 +4,17 @@
 
 #hello_python()
 
-def sum(num1,num2):
-    num3 = num1 + num2
-    print(num3)
+#def sum(num1,num2):
+#    num3 = num1 + num2
+#    print(num3)
 
-sum(15,25)
+#sum(15,25)
+
+def divideNummer(number):
+    return number / 2
+
+myList = [3,5,7,10,20,30]
+myResultList = []
+for num in myList:
+    myResultList.append(divideNummer(num))
+print(myResultList)
