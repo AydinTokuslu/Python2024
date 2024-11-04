01e2844c0c4b7e73e9c9208f257922491a18aadb
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-04 07:53:54 +0100

    day4

--- function.py
+++ function.py
@@ -1,5 +1,11 @@
-def hello_python():
-    print("hello")
-    print("python")
+#def hello_python():
+#    print("hello")
+#    print("python")
 
-hello_python()
+#hello_python()
+
+def sum(num1,num2):
+    num3 = num1 + num2
+    print(num3)
+
+sum(15,25)
