649168b433be300581f78fe67a8360652a9cd2e6
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-07 12:17:56 +0100

    day7

--- quiz3.py
+++ quiz3.py
@@ -1,25 +1,32 @@
+#1) Aşağıdaki kodun çıktısı ne olacaktır?
 #def toplama(a,b):
 #    print(a,b)
 
 #x = toplama(3,4)
 #print(x)
 
+#2) Aşağıdaki kodun çıktısı ne olacaktır?
 #def usselIslem(x=5,y=3):
 #    print(x ** y)
 
+#3) Aynı fonksiyonu aşağıdaki gibi çağırırsak çıktı ne olur?
 #usselIslem()
 
+#4) Aşağıdaki kodun çıktısı ne olacaktır?
 #def myLoop(*args):
 #    for element in args:
 #        print(element / 2)
 
 #myLoop(3,2,1,5,3,4)
 
-def myFunc(num):
-    return num ** 3
+#5) Aşağıdaki dizide belirtilen rakamları, myFunction fonksiyonuna tabi tutup, yeni bir dizi oluşturunuz
+#def myFunc(num):
+#    return num ** 3
 
-myList = [2,3,4,5,6]
-newList = []
-for i in myList:
-    newList.append(myFunc(i))
-print(newList)
+#myList = [2,3,4,5,6]
+#newList = []
+#for i in myList:
+#    newList.append(myFunc(i))
+#print(newList)
+
+#print(list(map(myFunc,myList)))
