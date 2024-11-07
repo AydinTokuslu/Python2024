434c9940a066fba9fee8f347d935edeeda3d1abd
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-07 09:20:05 +0100

    day7

--- quiz3.py
+++ quiz3.py
@@ -1,5 +1,16 @@
-def toplama(a,b):
-    print(a,b)
+#def toplama(a,b):
+#    print(a,b)
 
-x = toplama(3,4)
-print(x)
+#x = toplama(3,4)
+#print(x)
+
+#def usselIslem(x=5,y=3):
+#    print(x ** y)
+
+#usselIslem()
+
+def myLoop(*args):
+    for element in args:
+        print(element / 2)
+
+myLoop(3,2,1,5,3,4)
