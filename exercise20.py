943537c09d0e92e22e6cb1f9ea787fa6527827a2
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-09 19:38:27 +0100

    day9

---
+++ exercise20.py
@@ -0,0 +1,18 @@
+# Write a function called equal_strings. The function takes two
+# strings as arguments and compares them. If the strings are equal
+# (if they have the same characters and have equal length), it
+# should return True, if they are not, it should return False. For
+# example, ‘love’ and ‘evol’ should return True.
+
+def equal_strings(str1,str2):
+    if len(str1) == len(str2):
+        for i in str1:
+            #print(i)
+            if i not in str2:
+                return False
+        return True
+    else:
+        return False
+
+#print(equal_strings("love","evol"))
+print(equal_strings("alim","mila"))
