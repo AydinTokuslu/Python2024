93eb1c807f2892e70f3dfe81ee8ff74dc0c536d3
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-09 14:28:44 +0100

    day9

--- exercise16.py
+++ exercise16.py
@@ -3,3 +3,19 @@
 # example, if you pass ‘23569’ as an argument, your function
 # should return 9. Use list comprehension.
 
+def biggest_odd(str):
+    num = int(str)
+    new_num = []
+    for i in range(0,len(str)):
+        new_num.append(num%10)
+        num = int(num / 10)
+    return new_num
+
+def maximum(list):
+    max = 1
+    for i in list:
+        if i > max:
+            max = i
+    print(max)
+
+maximum(biggest_odd("23569"))
