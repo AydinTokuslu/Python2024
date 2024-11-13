600cd37c9b2583ecd965969fbb96f9814058ca4f
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-13 14:19:09 +0100

    day13

--- GIF_Creator.py
+++ GIF_Creator.py
@@ -4,17 +4,11 @@ import imageio
 # 1. Get Images
 cap = cv2.VideoCapture(1) # harici camera kullanıyorsak 1, bilgisayar kamerası ise 0
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("frame", frame)
-
 frames = []
 image_count = 0
 while True:
     ret, frame = cap.read()
-
     cv2.imshow("frame", frame)
-
     key = cv2.waitKey(0)
     if key == ord("a"):
         image_count += 1
@@ -22,15 +16,16 @@ while True:
         print("Adding new image:", image_count)
     elif key == ord("q"):
         break
-
 print("Images added: ", len(frames))
+
 
 print("Saving GIF file")
 with imageio.get_writer("smiling.gif", mode="I") as writer:
     for idx, frame in enumerate(frames):
         print("Adding frame to GIF file: ", idx + 1)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         writer.append_data(frame)
 
-rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 
 
