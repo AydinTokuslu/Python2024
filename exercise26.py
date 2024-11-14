9c9b5caff8a3ea9b70a99ae4c0bd17d69d6a96f7
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-14 17:16:02 +0100

    day14

---
+++ exercise26.py
@@ -0,0 +1,5 @@
+# Write a function called flat_list that takes one argument, a
+# nested list. The function converts the nested list into a one-dimension list.
+# For example [[2,4,5,6]] should return
+# [2,4,5,6].
+
