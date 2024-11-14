e909b083ae96ea236608bacab38ae996f9a2280f
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-14 17:42:54 +0100

    day14

--- exercise26.py
+++ exercise26.py
@@ -3,3 +3,13 @@
 # For example [[2,4,5,6]] should return
 # [2,4,5,6].
 
+newList = []
+
+def flat_list(list):
+    for i in list:
+        for j in i:
+            newList.append(j)
+    print(newList)
+
+nestedList = [[2,4,5,6]]
+flat_list(nestedList)
