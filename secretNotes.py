56b36f87fa6785d09442bdc71e769b7b9553d0a9
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-19 22:52:50 +0100

    day19

--- secretNotes.py
+++ secretNotes.py
@@ -1,47 +1,40 @@
 
 from cryptography.fernet import Fernet
 
-'''''
-message = "hello geeks"
 
-key = Fernet.generate_key()
-
-fernet = Fernet(key)
-
-encMessage = fernet.encrypt(message.encode())
-
-print("original string: ", message)
-print("encrypted string: ", encMessage)
-
-decMessage = fernet.decrypt(encMessage).decode()
-
-print("decrypted string: ", decMessage)
-
-
-'''
 key = Fernet.generate_key()
 fernet = Fernet(key)
 
 def save_encrypt():
+
     title = input("please enter your title = ")
     secret = input("please enter your secret = ")
     master_key = input("please enter your master key = ")
-
     encoded = fernet.encrypt(secret.encode())
-    return (encoded, master_key)
+    print(encoded)
+    decrypt(encoded, master_key)
 
 
 ## And then to decode it:
 def decrypt(encoded,master_key):
-    master_key2 = input("please enter your master key = ")
     while True:
-        if master_key2 == master_key:
-            decoded = fernet.decrypt(encoded).decode()
-
-        else:
-            print("please enter the correct master key")
+        try:
+            master_key2 = input("please enter your master key = ")
+            if master_key2 == master_key:
+                decoded = fernet.decrypt(encoded).decode()
+                print(decoded)
+                break
+            else:
+                falsch_decrypt(master_key2)
+                print("please enter the correct master key!!!!!")
+        except:
             continue
-        return decoded
 
-print(decrypt(save_encrypt()))
+def falsch_decrypt(str2):
+    encoded = fernet.encrypt(str2.encode())
+    print(encoded)
+
+save_encrypt()
+
+
 
