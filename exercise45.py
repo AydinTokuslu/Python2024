f8d1ed881c99804518c44aa4ba5334cc01ffd556
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-22 14:43:49 +0100

    day22

--- exercise45.py
+++ exercise45.py
@@ -1,11 +1,17 @@
-def average_calories():
-    calories = []
-    while True:
-        calori = input("please enter the calorie, or enter 'done' to exit : ")
-        if calori == "done":
-            break
-        else:
-            calories.append(int(calori))
-    return f"Average intake is {sum(calories)/len(calories)}"
+# Write a function called nested_lists that takes any number of
+# lists and creates a 2-dimensional nested list of lists. For example,
+# if you pass the following lists as arguments: [1, 2, 3, 5], [1, 2, 3,
+# 4], [1, 3, 4, 5].
+# Your code should return: [[1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]
 
-print(average_calories())
+def nested_lists(*args):
+    nested = []
+    for i in range(len(args)):
+        for j in args:
+            nested.append(j)
+        break
+    print(nested)
+
+
+list = [1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]
+nested_lists([1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5])
