003aae26fdbf3812544a82e75aa4a5ab9d265845
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-24 11:56:30 +0100

    day24

--- exercise53.py
+++ exercise53.py
@@ -5,4 +5,19 @@
 # number in this exact format: 9,877,632 as the largest number.
 
 def largest_number(list):
-    pass
+    nummer = ""
+    for i in list:
+        nummer += str(i)
+
+    nummer = ''.join(sorted(nummer, reverse = True))
+    numList = ""
+    sayi1 = int(nummer) % 1000
+    sayi2 = int(nummer) // 1000
+    sayi3 = sayi2 % 1000
+    sayi4 = sayi2 // 1000
+
+    return f"{sayi4},{sayi3},{sayi1}"
+
+
+list1 = [3, 67, 87, 9, 2]
+print(largest_number(list1))
