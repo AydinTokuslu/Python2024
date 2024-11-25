8fb26457fc6bed20cd36f38159c984f6169b0b3b
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-25 08:14:23 +0100

    day25

---
+++ exercise54.py
@@ -0,0 +1,19 @@
+# Write a function called middle_figure that takes two
+# parameters a, and b. The two parameters are strings. The
+# function joins the two strings and finds the middle element.
+# If the combined string has a middle element, the function should
+# return the element, otherwise, return ‘no middle figure’. Use
+# ‘make love’ as an argument for a and ‘not wars’ as an
+# argument for b. Your function should return ‘e’ as the middle
+# element. Whitespaces should be removed.
+
+def middle_figure(a, b):
+    c = a + b
+    if len(c) % 2 == 0 :
+        print("no middle figure")
+    else:
+        print(c[(len(c)//2)])
+
+a = "make love"
+b = "not warsse"
+middle_figure(a,b)
