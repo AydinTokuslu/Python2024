3ee4a3e0c0104bd82d0d482a812a9fcfabb228a9
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-26 17:15:53 +0100

    day26

--- exercise66.py
+++ exercise66.py
@@ -1,2 +1,19 @@
+# Write a function called count that takes one argument a string,
+# and returns a dictionary of how many times each element
+# appears in the string. For example, ‘hello’ should return:
+# {‘h’:1,’e’: 1,’l’:2, ‘o’:1}.
 
+
+def count(s):
+    dict = {}
+    for i in s:
+        if i not in dict.keys():
+            dict[i] = 1
+        else:
+            dict[i] += 1
+
+    print(dict)
+
+s = "hello boby"
+count(s)
 
