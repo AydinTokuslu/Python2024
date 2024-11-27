edf0834b038073185b977bcfc6840c03e2c3c304
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-27 11:07:20 +0100

    day27

--- exercise74.py
+++ exercise74.py
@@ -1,22 +1,50 @@
-from textblob import TextBlob
+# Create three classes of three car brands – Ford, BMW, and
+# Tesla. The attributes of the car's objects will be, model,
+# color, year, transmission, and whether the car is
+# electric or not (Boolean value.) Consider using inheritance
+# in your answer.
+# You will create one object for each car brand:
+# bmw1 : model: x6, Color: silver, Year: 2018, Transmission: Auto, Electric: False
+# tesla1: model: S, Color: beige, Year: 2017, Transmission: Auto, Electric: True
+# ford1 : model: focus, Color: white, Year: 2020, Transmission: Auto, Electric: False
+# You will create a class method, called print_cars that will be
+# able to print out objects of the class. For example, if you call the
+# method on the ford1 object of the Ford class, your function
+# should be able to print out car info in this exact format:
+# car_model = focus
+# Color = White
+# Year = 2020
+# Transmission = Auto
+# Electric = False
 
-def spelling_checker():
-    while True:
-        word = input('Enter a word : ')
-        # checking if the input word is correct
-        if word != TextBlob(word).correct():
-            # suggesting the correct word to the user
-            question = input(f'Did you mean '
-                            f'{TextBlob(word).correct()}?. '
-                            f'type yes/no :')
-            if question == 'yes':
-                break
-            else:
-                print('Please try again')
-        # if the word entered is correct
-        # the code breaks and returns word
-        elif word == TextBlob(word).correct():
-            break
-    return f'Your word is, {TextBlob(word).correct()}'
+class Cars:
+    def __init__(self, model, color, year, transmission, electric = False):
+        self.model = model
+        self.color = color
+        self.year = year
+        self.transmission = transmission
+        self.electric = electric
 
-print(spelling_checker())
+    def print_car(self):
+        return f"Car Model = {self.model}\nColor = {self.color} " \
+                f"\nYear = {self.year}\nTransmission = {self.transmission}" \
+                f"\nElectric = {self.electric}"
+
+class BMW(Cars):
+    def __init__(self, model, color, year, transmission, electric=False):
+        Cars.__init__(self, model, color, year, transmission, electric = electric)
+class Tesla(Cars):
+    def __init__(self, model, color, year, transmission, electric=False):
+        Cars.__init__(self, model, color, year, transmission, electric = electric)
+class Ford(Cars):
+    def __init__(self, model, color, year, transmission, electric=False):
+        Cars.__init__(self, model, color, year, transmission, electric = electric)
+
+ford1 = Ford("Focus", "White", 2017, "Auto", False)
+print(ford1.print_car())
+
+tesla1 = Tesla("S", "Grey", 2016, "Manual", True)
+print(tesla1.print_car())
+
+bmw1 = BMW('X6', 'silver', 2018, 'Auto', False)
+print(bmw1.print_car())
