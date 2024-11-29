1c85fa74e1504520b7988a1b7cb61e1d1b78bc49
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-29 07:56:46 +0100

    day29

--- exercise87.py
+++ exercise87.py
@@ -1,1 +1,22 @@
 # yazı-tura atarak yazı mı tura mı olduğunu bilmek
+
+import random
+
+yazi=0
+tura=0
+
+para_yuzeyi=["tura","yazi"]
+atilcak_para_sayisi=int(input("kaç kere atmak istiyorsunuz : "))
+
+while atilcak_para_sayisi>0:
+    para_ustu=random.choice(para_yuzeyi)
+    #print(para_ustu)
+    if para_ustu=="tura" or para_ustu=="Tura":
+        tura+=1
+        print("tura geldi!!")
+    else:
+        yazi+=1
+        print("yazi geldi!!!")
+    atilcak_para_sayisi-=1
+
+print("Tura sayısı : {}\nYazi sayısı : {}".format(tura,yazi))
