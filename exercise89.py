7d5152f2259b7a25c911df4b8e5561264a16bfea
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-29 08:18:04 +0100

    day29

---
+++ exercise89.py
@@ -0,0 +1,18 @@
+## Finding prime numbers from the entered number
+
+def prime(num):
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True
+
+
+num=int(input("please enter a number : "))
+prime_numbers=[]
+
+for i in range(2, num+1):
+    if prime(i):
+        prime_numbers.append(i)
+
+
+print(prime_numbers)
