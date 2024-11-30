758b7cb07f32817d5d984a18c4c30ced376e79ee
AydinTokuslu <aydintokuslu78@gmail.com>
2024-11-30 19:50:34 +0100

    day30

--- hackerRank_day7.py
+++ hackerRank_day7.py
@@ -1,2 +1,20 @@
 # Given an array,A , of N integers, print A's elements in reverse order as a single line of space-separated numbers.
 
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(arr[::-1])
+    test_string = ""
+    sorted(arr, key=lambda x: (-x[0], x[1]))
+    #for i in arr:
+    #    res = ''.join(sorted(i, reverse=True))
+    #print(str(res))
